9c2ae32554ef701568387e60877f9784b98f683c
Rosaline Scully <rosaline.scully@gmail.com>
2024-09-27 20:27:21 -0400

    Update LinkedNumberException.java

--- assignment_2/src/LinkedNumberException.java
+++ assignment_2/src/LinkedNumberException.java
@@ -1,5 +1,4 @@
 /**
- * CS1027B Assignment 2 rscully5
  * This is a special exception that can print messages that I set for errors.
  */
 public class LinkedNumberException extends RuntimeException {
